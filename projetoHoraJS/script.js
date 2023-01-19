486b7fe7080851e70409371013a4d7edd0ecb3ae
Arthur Ribeiro <110831236+arthuribeirojn@users.noreply.github.com>
2023-01-19 16:14:26 -0300

    Quebrando a cabeça

---
+++ projetoHoraJS/script.js
@@ -0,0 +1,19 @@
+
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('foto')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 16
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    img.src = 'dia.png'
+}else if (hora >= 12 && hora < 18) {
+    img.src = 'tarde.png'
+
+}else{
+    img.src = 'noite.png'
+
+}
+
+}
