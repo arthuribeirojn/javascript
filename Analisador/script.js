92dd5d8b6d1297f298bacfc25114f33ac82c7b47
Arthur Ribeiro <110831236+arthuribeirojn@users.noreply.github.com>
2023-01-26 11:10:42 -0300

    Finalizado analisador de números

--- Analisador/script.js
+++ Analisador/script.js
@@ -1,6 +1,6 @@
-let num = document.getElelmentById("fnum");
-let lista = document.getElementById("flista");
-let res = document.getElementById("res");
+let num = document.querySelector("input#fnum");
+let lista = document.querySelector("select#flista");
+let res = document.querySelector("div#res");
 let valores = [];
 
 function isNumero(n) {
@@ -21,8 +21,40 @@ function inLista(n, l) {
 
 function adicionar() {
   if (isNumero(num.value) && !inLista(num.value, valores)) {
-    window.alert("OK");
+    valores.push(Number(num.value));
+    let item = document.createElement("option");/*Criar option no HTML*/
+    item.text = `Valor ${num.value} adicionado.`;
+    lista.appendChild(item);/*Acerescentar filho na lista*/
+    res.innerHTML = "";
   } else {
     window.alert("Valor inválido ou já encontra na lista.");
+  }
+  num.value = "";
+  num.focus();
+}
+
+function finalizar() {
+  if (valores.length == 0) {
+    window.alert("Adicione valores antes de finalizar!");
+  } else {
+    let tot = valores.length;
+    let maior = valores[0];
+    let menor = valores[0];
+    let pos = 0;
+    let soma = 0;
+    let media = 0;
+    for (let pos in valores) {
+      soma += valores[pos];
+
+      if (valores[pos] > maior) maior = valores[pos];
+    }
+    if (valores[pos] < menor) menor = valores[pos];
+    media = soma / tot;
+    res.innerHTML = "";
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+    res.innerHTML += `<p>O maior número é ${maior}.</p>`;
+    res.innerHTML += `<p>O menor número é ${menor}.</p>`;
+    res.innerHTML += `<p>A soma total da lista ${soma}.</p>`;
+    res.innerHTML += `<p>A média da lista é ${media}.</p`;
   }
 }
